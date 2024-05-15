e82d792a967e8dfaeb5980d1494daf17aa18c638
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-05-15 15:05:13 -0400

    les sections

--- assets/js/pages/quiz.js
+++ assets/js/pages/quiz.js
@@ -1,58 +1,68 @@
-import { init as navigationInit } from "../modules/navigation.js";
-import { questions, messages } from "../donnees/questionsQuiz.js";
-import Quiz from "../classes/Quiz.js";
+// import { init as navigationInit } from "../modules/navigation.js";
+// import { questions, messages } from "../donnees/questionsQuiz.js";
+// import Quiz from "../classes/Quiz.js";
 
-let reponsesQuiz = [];
-let indexQuestionCourante = 0;
-const template = document.querySelector("template#question");
-const quizConteneur = document.querySelector(".quiz-conteneur");
-const quizMessageConteneur = document.querySelector(".quiz-message-conteneur");
+// let reponsesQuiz = [];
+// let indexQuestionCourante = 0;
+// const template = document.querySelector("template#question");
+// const quizConteneur = document.querySelector(".quiz-conteneur");
+// const quizMessageConteneur = document.querySelector(".quiz-message-conteneur");
+
+// function init() {
+//   navigationInit();
+//   new Quiz();
+
+//   demarrerQuiz();
+//   afficherMessage("Bienvenue au quiz!");
+
+//   enregistrerPositionLocalStorage();
+//   enregistrerReponsesLocalStorage();
+// }
+
+// function demarrerQuiz() {
+//   questions.forEach(function (question) {
+//     afficherQuestion(question);
+//   });
+// }
+
+// function afficherQuestion(question) {
+//   let clone = template.content.cloneNode(true);
+//   quizConteneur.appendChild(clone);
+
+//   const questionHTML = quizConteneur.lastElementChild;
+//   const titre = questionHTML.querySelector(".question__titre");
+//   const options = questionHTML.querySelector(".question__options");
+
+//   titre.textContent = question.question;
+
+//   let boutonsReponses = "";
+//   question.reponses.forEach(function (reponse) {
+//     boutonsReponses += `<li class="bouton">${reponse}</li>`;
+//   });
+//   options.innerHTML = boutonsReponses;
+// }
+
+// function afficherMessage(message) {
+//   quizMessageConteneur.textContent = message;
+// }
+
+// function enregistrerReponsesLocalStorage() {
+//   const reponsesString = JSON.stringify(reponsesQuiz);
+//   localStorage.setItem("quizReponses", reponsesString);
+// }
+
+// function enregistrerPositionLocalStorage() {
+//   let positionString = JSON.stringify(indexQuestionCourante);
+//   localStorage.setItem("quizPosition", positionString);
+// }
+// init();
+
+import { init as navigationInit } from "../modules/navigation.js";
+import Quiz from "../classes/Quiz.js";
 
 function init() {
   navigationInit();
-  /* new Quiz(); */
-
-  demarrerQuiz();
-  afficherMessage("Bienvenue au quiz!");
-
-  enregistrerPositionLocalStorage();
-  enregistrerReponsesLocalStorage();
+  let nouveauQuiz = new Quiz();
 }
 
-function demarrerQuiz() {
-  questions.forEach(function (question) {
-    afficherQuestion(question);
-  });
-}
-
-function afficherQuestion(question) {
-  let clone = template.content.cloneNode(true);
-  quizConteneur.appendChild(clone);
-
-  const questionHTML = quizConteneur.lastElementChild;
-  const titre = questionHTML.querySelector(".question__titre");
-  const options = questionHTML.querySelector(".question__options");
-
-  titre.textContent = question.question;
-
-  let boutonsReponses = "";
-  question.reponses.forEach(function (reponse) {
-    boutonsReponses += `<li class="bouton">${reponse}</li>`;
-  });
-  options.innerHTML = boutonsReponses;
-}
-
-function afficherMessage(message) {
-  quizMessageConteneur.textContent = message;
-}
-
-function enregistrerReponsesLocalStorage() {
-  const reponsesString = JSON.stringify(reponsesQuiz);
-  localStorage.setItem("quizReponses", reponsesString);
-}
-
-function enregistrerPositionLocalStorage() {
-  let positionString = JSON.stringify(indexQuestionCourante);
-  localStorage.setItem("quizPosition", positionString);
-}
 init();
